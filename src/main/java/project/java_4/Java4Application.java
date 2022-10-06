4aad86fb4777a4633a30a60eb7c24d6f2eac6a9b
s4nk0 <91661956+s4nk0@users.noreply.github.com>
2022-10-06 22:03:25 +0600

    first commit

---
+++ src/main/java/project/java_4/Java4Application.java
@@ -0,0 +1,13 @@
+package project.java_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java4Application.class, args);
+    }
+
+}
---
+++ src/test/java/project/java_4/Java4ApplicationTests.java
@@ -0,0 +1,13 @@
+package project.java_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
